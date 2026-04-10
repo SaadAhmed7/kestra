bb00ab20710f06b1df62430d31ba0bd4d02a3213
Roman Acevedo <roman.acevedo62@gmail.com>
2026-04-10 16:38:15 +0200

    fix(core,cli,executor,webserver): replace deprecated Await with Awaitility in production code
    Migrate all 12 production call sites from the deprecated io.kestra.core.utils.Await
    to org.awaitility.Awaitility, which is already an api dependency of core:
    - Indefinite polls (server lifecycle, Docker output drain): await().forever().until(...)
    - Timed polls with error return (ServiceRegistry): await().atMost().pollInterval().until(...)
      with catch (ConditionTimeoutException) instead of (TimeoutException)
    - Timed polls with value return (ExecutionController): await().atMost().pollInterval()
      .until(supplier, Objects::nonNull)
    - Remove all @SuppressWarnings("deprecation") annotations that were silencing the Await usage

--- executor/src/main/java/io/kestra/executor/DefaultExecutor.java
+++ executor/src/main/java/io/kestra/executor/DefaultExecutor.java
@@ -39,6 +39,8 @@ import io.kestra.core.server.ServiceStateChangeEvent;
 import io.kestra.core.server.ServiceType;
 import io.kestra.core.services.*;
 import io.kestra.core.utils.*;
+
+import org.awaitility.Awaitility;
 import io.kestra.executor.handler.*;
 import io.kestra.plugin.core.trigger.Webhook;
 
@@ -195,7 +197,6 @@ public class DefaultExecutor extends AbstractService implements Executor {
     }
 
     @Override
-    @SuppressWarnings("deprecation")
     public void run() {
         // listen to executor related queues
         this.queueSubscribers.addFirst(this.executionQueue.subscriber().subscribe(this::executionQueue));
@@ -270,7 +271,7 @@ public class DefaultExecutor extends AbstractService implements Executor {
         Thread.ofVirtual().name("executor-delay-exception-watcher").start(
             () ->
             {
-                Await.until(executionDelayFuture::isDone);
+                Awaitility.await().forever().until(executionDelayFuture::isDone);
 
                 try {
                     executionDelayFuture.get();
@@ -291,7 +292,7 @@ public class DefaultExecutor extends AbstractService implements Executor {
         Thread.ofVirtual().name("executor-sla-monitor-exception-watcher").start(
             () ->
             {
-                Await.until(monitorSLAFuture::isDone);
+                Awaitility.await().forever().until(monitorSLAFuture::isDone);
 
                 try {
                     monitorSLAFuture.get();
